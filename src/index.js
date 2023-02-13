8c0551e885385dc6b81f13d9051c26b04862bf1e
paseka-alex <101744576+paseka-alex@users.noreply.github.com>
2023-02-13 16:44:00 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+async function fetch_info(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+  document.getElementById("result").innerHTML = data["count"];
+  document.getElementById("asjson").innerHTML = JSON.stringify(data);
+}
+
+document.getElementById("button").addEventListener("click", function () {
+  let input = document.getElementById("keyword");
+  let keyword = input.value;
+  let URL = `https://ll.thespacedevs.com/2.2.0/launch/?mode=list&search=${keyword}`;
+  fetch_info(URL);
+});
